f30920b6f8a625a9fb5c84abe08ab4b0ec255016
yenlinh000 <yenlinh.nc21@gmail.com>
2021-05-10 19:48:06 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,216 @@
+<html lang="en">
+
+<head>
+	<meta http-equiv="X-Clacks-Overhead" content="GNU Terry Pratchett" />
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Just Psychology | Free interactive 3D for body language</title>
+<meta name="generator" content="Jekyll v3.9.0" />
+<meta property="og:title" content="JustPsychology"/>
+<meta property="og:locale" content="en_UK" />
+<meta name="description" content="Free character posing tool for figure drawing, comic illustration, sketching, bla bla"/>
+<meta property="og:description" content="like the description"/>
+<link rel="canonical" href="index.php" />
+<meta property="og:url" content="index.php" />
+<meta name="twitter:card" content="summary" />
+<meta property="twitter:title" content="JustPsychology" />
+<script type="application/ld+json">
+	
+</script>
+	<meta name="keywords" content="bla bla" />
+	<meta property="og:image" content="/images/snippet.png">
+	<meta property="og:type" content="website" />
+
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:site" content="$JustPsychology">
+	<meta name="twitter:title" content="JustPsychology">
+	<meta name="twitter:image" content="/images/snippet.png">
+	<meta name="twitter:description" content="Free character">
+	<meta name="twitter:creator" content="@justpsychology">
+
+	<link type="application/atom+xml" rel="alternate" href="index.php" title="JustPsychology" />
+
+	<link rel="stylesheet" type="text/css" href="/css/screen.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicons-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicons-16x16.png">
+	<link rel="manifest" href="/images/favicons/site.webmanifest">
+	<link rel="mask-icon" href="/images/favicons/browserconfig.xml">
+	<link rel="shortcut-icon" href="/images/favicons/safari-pinned-tab.svg" color="#01c9d7">
+	<meta name="msapplication-TileColor" content="#01c9d7">
+	<meta name="msapplication-config" content="/images/favicons/browserconfig.xml">
+	<meta name="theme-color" content="#ffffff">
+
+	<script async defer data-domain="justpsychology.me" src="https://plausible.io/js/plausible.js"></script>
+</head>
+
+<body>
+	<header>
+		<div class="container">
+			<div class="navbar">
+				<a href="/" title="JustPsychology">
+					<img src="/images/logo-blackish.png" alt="JustPsychology" width="95" height="45" />
+					Just Pyschology
+				</a>
+			</div>
+		</nav>
+	<a class="nav-toggle" id="open-nav" href="#" title="Nav"></a>
+
+	<a href="/features/" class="" title="Features">Features</a>
+
+	<a href="/features/" class="" title="Pricing">Pricing</a>
+
+	<a href="/features/" class="" title="Gallery">Gallery</a>
+
+	<a href="/features/" class="" title="Learn">Learn</a>
+</nav>
+</div>
+</header>
+
+	<section class="hero">
+		<div class="container">
+			<div class="left-column">
+				<h1 class="title">
+					Breathe life into your soul.
+				</h1>
+				<h2 class="subtitle">
+					Give depth to your characters with the best pose reference tool on the web.
+				</h2>
+
+				<a href="index.php" target="_blank" rel="noopener noreferrer" title="Webapp">
+					<button class="button">
+						Open the Webapp
+					</button>
+				</a>
+				<small class="speech-bubble">It's free!</small>
+				<p>
+					Or download the 
+					<a href="https://justpsychology" download>Mac</a>
+					or
+					<a href="https://justpsychology" download>Windows</a>
+				</p>
+			</div>
+			<div class="right-column">
+				<div class="image">
+					<div class="toolbar">
+						<span></span><span></span><span></span>
+					</div>
+					<img src="/images/heros/sam1.png" alt="App image" class="sam back" />
+					<img src="/images/heros/sam2.png" alt="App image" class="sam mid" />
+					<img src="/images/heros/sam3.png" alt="App image" class="sam fore" />
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="second-hero">
+		<a href="/features/" title="Features">
+			<button class="button pink">Check out the features</button>
+		</a>
+		<br>
+		<br>
+		<a href="https://twitter.com/TheSpiderBugle" target="_blank">
+			<img src="images/heros/spidey-bugle.png" alt="Spiderman" />
+		</a>
+	</section>
+
+	<script>
+	document.querySelectorAll(".sam").forEach((element, i) => {
+		const timeout = (i+1)*1000;
+		setTimeout(() => element.classList.add('show'), timeout);
+	});
+
+	window.onscroll = () => {
+		const element = document.querySelector('.sam.mid');
+		element.classList.toggle("faded", element.scrollTop > 70 || document.documentElement.scrollTop > 70);
+	};
+	</script>
+
+	<section class="testimonials">
+		<h2 class="center-text">
+			What will you create?
+		</h2>
+		<div class="masonry">
+
+			<li>
+				<a href="https://twitter.com/CarlzSays/status/1296982854749036544" target="_blank" title="@carlzsays">
+					<p class="header">@carlzsays</p>
+					<img src="/images/gallery/carlzsays1.jpg" alt="@carlzsays" />
+				</a>
+			</li>
+
+			<li>
+				<a href="https://twitter.com/CarlzSays/status/1296982854749036544" target="_blank" title="@carlzsays">
+					<p class="header">@carlzsays</p>
+					<img src="/images/gallery/carlzsays1.jpg" alt="@carlzsays" />
+				</a>
+			</li>
+
+			<li>
+				<a href="https://twitter.com/CarlzSays/status/1296982854749036544" target="_blank" title="@carlzsays">
+					<p class="header">@carlzsays</p>
+					<img src="/images/gallery/carlzsays1.jpg" alt="@carlzsays" />
+				</a>
+			</li>
+
+			<li>
+				<a href="https://twitter.com/CarlzSays/status/1296982854749036544" target="_blank" title="@carlzsays">
+					<p class="header">@carlzsays</p>
+					<img src="/images/gallery/carlzsays1.jpg" alt="@carlzsays" />
+				</a>
+			</li>
+
+
+			<li>
+				<a href="https://twitter.com/CarlzSays/status/1296982854749036544" target="_blank" title="@carlzsays">
+					<p class="header">@carlzsays</p>
+					<img src="/images/gallery/carlzsays1.jpg" alt="@carlzsays" />
+				</a>
+			</li>
+
+			<li>
+				<a href="https://twitter.com/CarlzSays/status/1296982854749036544" target="_blank" title="@carlzsays">
+					<p class="header">@carlzsays</p>
+					<img src="/images/gallery/carlzsays1.jpg" alt="@carlzsays" />
+				</a>
+			</li>
+		</div>
+
+		<div class="center-text">
+			<br>
+			<br>
+			<br>
+			<a href="/gallery" title="gallery">
+				<button class="button pink">
+					Check out the gallery
+				</button>
+			</a>
+			<br>
+			<br>
+			<br>
+		</div>
+		
+	</section>
+
+	<footer>
+		<div class="container">
+			<h3>follow us...</h3>
+			<ul class="social-links">
+
+				<li>
+					<a target="_blank" rel="noopener noreferrer" 
+					href="index.php"
+					>
+
+
+
+
+	</section>
+
+
+
+	</header>
